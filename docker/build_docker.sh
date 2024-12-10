015aee195192bb2c0d5adfc081ecd9144220acff
Maximilian Leitenstern <maxi.leitenstern@tum.de>
2024-12-10 09:57:34 +0100

    fix docker push

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-docker build -f $SCRIPT_DIR/Dockerfile -t pointcloudcrafter:latest $SCRIPT_DIR/..
+docker build -f $SCRIPT_DIR/Dockerfile -t ghcr.io/tumftm/pointcloudcrafter:latest $SCRIPT_DIR/..
